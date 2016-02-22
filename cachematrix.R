72f2e5e75368711024b71df34b7daabcde8aa2d6
CRichtfort <crichtfort@yahoo.com>
2016-02-21 22:22:20 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## System for caching the inverse (solve(x)) of a matrix
 
-## Write a short comment describing this function
+
+## Given a matrix, creates a cached copy 
+## (for comparison to current matrix to detect changes)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+CacheMatrix <<- x
+StoredInverse <<- NULL
 }
 
 
-## Write a short comment describing this function
+## Given a matrix, checks for a stored inverse to the cached matrix.
+## If there is no stored inverse, compute and store one.
+## If the current matrix does not match the cached copy ...
+## a new reference copy is stored and a new inverse is cached.
 
 cacheSolve <- function(x, ...) {
+## Return a matrix that is the inverse of 'x'
+
+if(is.null(CacheMatrix)){
+makeCacheMatrix(x)
+StoredInverse<<-NULL
+}else if(!identical(x,CacheMatrix)){
+makeCacheMatrix(x)
+StoredInverse<<-NULL
+}
+
+
+if(is.null(StoredInverse)){ 
+StoredInverse<<-solve(CacheMatrix)
+}
+StoredInverse
         ## Return a matrix that is the inverse of 'x'
 }
